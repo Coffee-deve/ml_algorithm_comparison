3606878169c47baceaeb12f05957fb7dd226201f
Patrycja <patrycja.nazim@gmail.com>
2025-12-16 15:32:40 +0100

    update

--- src/ml_algorithm_comparison/knnCustom.py
+++ src/ml_algorithm_comparison/knnCustom.py
@@ -1,81 +1,58 @@
-"""
-knnCustom.py
-
-Custom implementation of the k-Nearest Neighbors (KNN) algorithm.
-"""
-
 import numpy as np
 from collections import Counter
 
 
 class KNNCustom:
-    """
-    Custom K-Nearest Neighbors classifier.
-
-    Parameters
-    ----------
-    kNeighbors : int
-        Number of nearest neighbors used for voting.
-    """
-
-    def __init__(self, kNeighbors=3):
-        self.kNeighbors = kNeighbors
-
-    def fit(self, trainData, trainLabels):
+    def __init__(self, kNeighbors=3, distanceMetric="euclidean", minkowskiP=2):
         """
-        Fit the model using training data.
+        Custom K-Nearest Neighbors classifier.
 
         Parameters
         ----------
-        trainData : array-like
-            Feature vectors of training samples.
-        trainLabels : array-like
-            Class labels for training samples.
+        kNeighbors : int
+            Number of nearest neighbors.
+        distanceMetric : str
+            Distance metric to use: 'euclidean', 'manhattan', or 'minkowski'.
+        minkowskiP : int
+            Power parameter for Minkowski distance.
         """
+        self.kNeighbors = kNeighbors
+        self.distanceMetric = distanceMetric
+        self.minkowskiP = minkowskiP
+
+    def fit(self, trainData, trainLabels):
         self.trainData = np.array(trainData)
         self.trainLabels = np.array(trainLabels)
 
-    def _euclideanDistance(self, a, b):
-        """
-        Compute Euclidean distance between two points.
+    def _distance(self, a, b):
+        if self.distanceMetric == "euclidean":
+            return np.sqrt(np.sum((a - b) ** 2))
 
-        Parameters
-        ----------
-        a : array-like
-            First point.
-        b : array-like
-            Second point.
+        elif self.distanceMetric == "manhattan":
+            return np.sum(np.abs(a - b))
 
-        Returns
-        -------
-        float
-            Euclidean distance.
-        """
-        return np.sqrt(np.sum((a - b) ** 2))
+        elif self.distanceMetric == "minkowski":
+            return np.sum(np.abs(a - b) ** self.minkowskiP) ** (1 / self.minkowskiP)
+
+        else:
+            raise ValueError(
+                "Invalid distanceMetric. Use 'euclidean', 'manhattan', or 'minkowski'."
+            )
 
     def predict(self, testData):
-        """
-        Predict class labels for test data.
-
-        Parameters
-        ----------
-        testData : array-like
-            Feature vectors to classify.
-
-        Returns
-        -------
-        numpy.ndarray
-            Predicted class labels.
-        """
         predictions = []
 
         for point in testData:
             distances = [
-                self._euclideanDistance(point, trainPoint)
+                self._distance(point, trainPoint)
                 for trainPoint in self.trainData
             ]
-            nearest = np.argsort(distances)[:self.kNeighbors]
-            labels = self.trainLabels[nearest]
-            predictions.append(Counter(labels).most_common(1)[0][0])
+
+            nearestIndices = np.argsort(distances)[:self.kNeighbors]
+            nearestLabels = self.trainLabels[nearestIndices]
+
+            predictions.append(
+                Counter(nearestLabels).most_common(1)[0][0]
+            )
 
         return np.array(predictions)
